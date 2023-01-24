11f0c36ca5ccf460d69591cc11753dd87ea69331
mad-of-gravity <mad.of.gravity@gmail.com>
2023-01-24 23:52:28 +0200

    Body background color was changed to #0f0f29

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 
+
 function App() {
   return (
     <div className="App">
@@ -12,6 +13,8 @@ function App() {
       <div class="container is-fullhd">
         <div class="notification">
           Edit the <code>./src</code> folder to add components.
+          <p>Find what are you looking for</p>
+          <img src="images/illustrations/meditating-woman.png" alt="woman"/>
         </div>
       </div>
     </div>
