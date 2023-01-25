0ae28c8998144a26fa54d4c942024e9570ddd594
mad-of-gravity <mad.of.gravity@gmail.com>
2023-01-25 13:19:00 +0200

    the hover selector has been added to the style function

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,24 +1,55 @@
-const getContainedButtonStyle = ({color, display}) => {
-    const defaultColors = {
-        primary: "#4838D1",
-        transparent: "transparent",
+import { useState } from "react";
+
+export const Button = ({
+  color = "primary",
+  variant = "contained",
+  display = "inline-block",
+  children,
+  ...props
+}) => {
+    //This will contain the hover status value
+    const [isHover, setIsHover] = useState(false);
+
+    //Hover handlers
+    const handleMouseEnter = () => {
+        setIsHover(true);
     }
+
+    const handleMouseLeave = () => {
+        setIsHover(false);
+    }
+
+
+  const getContainedButtonStyle = ({ color, display, ...props }) => {
+    const defaultColors = {
+      primary: "#4838D1",
+      transparent: "transparent",
+    };
 
     return {
-        padding: "16px 32px",
-        color: "#FFFFFF",
-        backgroundColor: (!defaultColors.hasOwnProperty(color)) ? color : defaultColors[color], //If the object does not contain color property -> return color 
-        border: "none",
-        borderRadius: "8px",
-        display: display,
-        width: (display === "block") ? "100%" : "inherit",
-    }
-}
+      padding: "16px 32px",
+      color: "#FFFFFF",
+      backgroundColor: !defaultColors.hasOwnProperty(color) //If the object does not contain color property -> return color
+        ? color
+        : defaultColors[color],
+      border: "none",
+      borderRadius: "8px",
+      display: display,
+      textAlign: "center",
+      fontWeight: props.fontWeight ? props.fontWeight : "normal",
+      width: display === "block" ? "100%" : props.width,
+      opacity: isHover ? "0.7" : "1",
+    };
+  };
 
-export const Button = ({color = "primary", variant="contained", display="inline-block", children}) => {
-    return (
-        <button style={getContainedButtonStyle({color, display})}>
-            {children}
-        </button>
-    )
-}
+  return (
+    <button
+      style={getContainedButtonStyle({ color, display, ...props })}
+      onClick={props.onClick}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      {children}
+    </button>
+  );
+};
