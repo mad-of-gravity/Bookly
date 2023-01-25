2f9c1d340dbd897855d658dddf5db2ddd3da2adf
mad-of-gravity <mad.of.gravity@gmail.com>
2023-01-25 18:14:12 +0200

    Add margin rule to dots container

--- src/components/Pages/OnBoardingPage/OnBoarding.jsx
+++ src/components/Pages/OnBoardingPage/OnBoarding.jsx
@@ -2,7 +2,7 @@ import { createUseStyles } from "react-jss";
 import Button from "../../ButtonComponent/Button";
 import Dot from "../../DotComponent/Dot";
 
-
+//The page styles used
 const useStyles = createUseStyles({
   pageContainer: {
     width: "90%",
@@ -16,11 +16,14 @@ const useStyles = createUseStyles({
   }
 });
 
+
 const OnBoarding = () => {
   const classes = useStyles();
   return (
     <div className={classes.pageContainer}>
       <img src="meditating-woman.png" alt="Meditating women" width="30%" height="30%"/>
+
+      ${//Dots that change the title and content of the page}
       <div className={classes.dots}>
         <Dot diameter="12px" margin="0 4px" color="#4838D1"/>
         <Dot diameter="12px" margin="0 4px" color="#2E2E5D"/>
