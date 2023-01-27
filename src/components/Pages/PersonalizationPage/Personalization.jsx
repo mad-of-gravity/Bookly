47e442a1cf0754357880f96aa8fb702acebe5594
mad-of-gravity <mad.of.gravity@gmail.com>
2023-01-27 22:19:23 +0200

    Input component has been created

---
+++ src/components/Pages/PersonalizationPage/Personalization.jsx
@@ -0,0 +1,10 @@
+import { createUseStyles } from "react-jss";
+import Input from "../../InputComponent/Input";
+
+const Personalization = ({props}) => {
+    return (
+        <Input placeholder="Placeholder"/>
+    );
+}
+
+export default Personalization;
